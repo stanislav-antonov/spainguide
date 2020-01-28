00c75c4d0a07084ecbccbbc54fe07aa1faabbf21
Stanislav Antonov <stanislav.antonov@siemens.com>
2020-01-28 16:04:38 +0100

    initital commit

---
+++ src/test/java/com/pse/spainguide/SpainguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pse.spainguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpainguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
