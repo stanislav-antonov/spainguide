0e24d919746c3a024172faa470f1c09ca1d6e370
Stanislav Antonov <stanislav.antonov@siemens.com>
2020-02-10 13:24:51 +0100

    Go on

--- src/main/java/com/pse/spainguide/configuration/MvcConfiguration.java
+++ src/main/java/com/pse/spainguide/configuration/MvcConfiguration.java
@@ -1,6 +1,7 @@
 package com.pse.spainguide.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,5 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfiguration implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/admin").setViewName("admin-spa/index");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
     }
 }
